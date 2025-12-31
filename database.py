547f7116d4fafc22da38343f54144bc5e77dcd4e
kinjal kathrecha <165824478+kinjalkathrecha@users.noreply.github.com>
2025-12-31 16:39:15 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:kinjal123@localhost:5432/job_scraper_db"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
 sessionlocal=sessionmaker(bind=engine)
 base=declarative_base()
